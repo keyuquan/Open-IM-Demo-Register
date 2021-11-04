ca512ed3dfc8d32790dd0d1a1c4915ce5c8e7952
陈阳 <775517776@qq.com>
2021-11-04 15:13:18 +0800

    Remove Chinese

--- utils/uid.go
+++ utils/uid.go
@@ -11,12 +11,12 @@ import (
 
 func NumUidGenerator() string {
 	timeNum := strconv.FormatInt(time.Now().UnixNano()/100, 10)
-	//当前id是秒的时间戳+纳秒时间戳最后两位
+
 	id := timeNum[:10] + timeNum[len(timeNum)-2:]
 	return id
 }
 
-// EncryptPassword 密码加密
+// EncryptPassword
 func EncryptPassword(password string) string {
 	generatePassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
@@ -25,7 +25,7 @@ func EncryptPassword(password string) string {
 	return string(generatePassword)
 }
 
-// CheckPasswordIsRight 检查密码是否正确
+// CheckPasswordIsRight
 func CheckPasswordIsRight(password string, hashedPassword string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 }
